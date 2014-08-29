60eda4389c51404bcea533351208287e61c5c36f
llaull <laul@llovem.eu>
2014-08-29 11:18:16 +0200

    avec com

--- dataGridViewMysql2/Form1.Designer.cs
+++ dataGridViewMysql2/Form1.Designer.cs
@@ -32,17 +32,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
             this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
             this.bindingNavigator1.SuspendLayout();
@@ -53,7 +53,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 45);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(856, 380);
+            this.dataGridView1.Size = new System.Drawing.Size(644, 380);
             this.dataGridView1.TabIndex = 0;
             // 
             // bindingNavigator1
@@ -80,9 +80,34 @@
             this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
             this.bindingNavigator1.Name = "bindingNavigator1";
             this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
-            this.bindingNavigator1.Size = new System.Drawing.Size(880, 25);
+            this.bindingNavigator1.Size = new System.Drawing.Size(665, 25);
             this.bindingNavigator1.TabIndex = 1;
             this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Ajouter nouveau";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(37, 22);
+            this.bindingNavigatorCountItem.Text = "de {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Nombre total d\'éléments";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Supprimer";
             // 
             // bindingNavigatorMoveFirstItem
             // 
@@ -116,16 +141,9 @@
             this.bindingNavigatorPositionItem.Text = "0";
             this.bindingNavigatorPositionItem.ToolTipText = "Position actuelle";
             // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(37, 22);
-            this.bindingNavigatorCountItem.Text = "de {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Nombre total d\'éléments";
-            // 
             // bindingNavigatorSeparator1
             // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
             this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // bindingNavigatorMoveNextItem
@@ -148,32 +166,14 @@
             // 
             // bindingNavigatorSeparator2
             // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
             this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Ajouter nouveau";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorDeleteItem.Text = "Supprimer";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 437);
+            this.ClientSize = new System.Drawing.Size(665, 437);
             this.Controls.Add(this.bindingNavigator1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
--- dataGridViewMysql2/Form1.cs
+++ dataGridViewMysql2/Form1.cs
@@ -24,8 +24,6 @@ namespace dataGridViewMysql2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: cette ligne de code charge les données dans la table 'dataSet1.DataTable'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
-            //this.dataTableTableAdapter.Fill(this.dataSet1.DataTable);
             connex = new MySqlConnection("Server=localhost;Database=app_domobox;Uid=root;Pwd=;");
             dataAdap = new MySqlDataAdapter();
             dataAdap.SelectCommand = connex.CreateCommand();
@@ -33,6 +31,7 @@ namespace dataGridViewMysql2
             dataAdap.InsertCommand = connex.CreateCommand();
             dataAdap.DeleteCommand = connex.CreateCommand();
 
+            // mapping des collones avec la table MySQL appelé 
             DataTableMapping tableMapping = new DataTableMapping();
             tableMapping.SourceTable = "Table";
             tableMapping.DataSetTable = "TableModule";
@@ -44,11 +43,14 @@ namespace dataGridViewMysql2
             tableMapping.ColumnMappings.Add("module_type", "module_type");
             dataAdap.TableMappings.Add(tableMapping);
 
+            //adapteur pour le SELECT
             dataAdap.SelectCommand.CommandText = "SELECT * FROM app_domobox.domotic_sensor_module;";
-            //dataAdap.DeleteCommand.CommandText = "DELETE FROM app_domobox.domotic_sensor_module WHERE ([id] = @id);";
+
+            //adapteur pour le DELETE
             dataAdap.DeleteCommand.CommandText = "DELETE FROM app_domobox.domotic_sensor_module WHERE id=@id;";
             dataAdap.DeleteCommand.Parameters.Add(new MySqlParameter("@id", MySqlDbType.Int32, 0, "id"));
 
+            //adapteur pour l'UPDATE
             dataAdap.UpdateCommand.CommandText = "UPDATE app_domobox.domotic_sensor_module SET module_ref=@module_ref,module_nom=@module_nom,module_emplacement=@module_emplacement,nrf_id=@nrf_id,module_type=@module_type WHERE id=@id;";
             dataAdap.UpdateCommand.Parameters.Add(new MySqlParameter("@id", MySqlDbType.Int32, 0, "id"));
             dataAdap.UpdateCommand.Parameters.Add(new MySqlParameter("@module_ref", MySqlDbType.Int32, 0, "module_ref"));
@@ -57,6 +59,7 @@ namespace dataGridViewMysql2
             dataAdap.UpdateCommand.Parameters.Add(new MySqlParameter("@nrf_id", MySqlDbType.Int32, 0, "nrf_id"));
             dataAdap.UpdateCommand.Parameters.Add(new MySqlParameter("@module_type", MySqlDbType.VarChar, 0, "module_type"));
 
+            //adapteur pour l'INSERT
             dataAdap.InsertCommand.CommandText = "INSERT INTO app_domobox.domotic_sensor_module (module_ref,module_nom,module_emplacement,nrf_id,module_type) VALUES (@module_ref,@module_nom,@module_emplacement,@nrf_id,@module_type);";// select LAST_INSERT_ID() AS id;
             dataAdap.InsertCommand.Parameters.Add(new MySqlParameter("@id", MySqlDbType.Int32, 0, "id")).Direction = ParameterDirection.Output;
             dataAdap.InsertCommand.Parameters.Add(new MySqlParameter("@module_ref", MySqlDbType.Int32, 0, "module_ref"));
@@ -65,36 +68,53 @@ namespace dataGridViewMysql2
             dataAdap.InsertCommand.Parameters.Add(new MySqlParameter("@nrf_id", MySqlDbType.Int32, 0, "nrf_id"));
             dataAdap.InsertCommand.Parameters.Add(new MySqlParameter("@module_type", MySqlDbType.VarChar, 0, "module_type"));
             
+            //attache le resultat du SELECT dans un DataTable
             DataTable table = new DataTable("TableModule");
             dataAdap.Fill(table);
+
+            //attache la DataTable dans le Navigator
             BindingSource binder = new BindingSource(table,null);
             dataGridView1.DataSource = binder;
             bindingNavigator1.BindingSource = binder;
-            //dataAdap.RowUpdating += new MySqlRowUpdatingEventHandler(adapRowUpdatingEventHandler);
+
+            //function sur les actions effectué sur le DataTable
             dataAdap.RowUpdated += dataAdap_RowUpdated;
             table.RowChanged += table_RowChanged;
             table.RowDeleted += table_RowChanged;
+
+            //dataAdap.RowUpdating += new MySqlRowUpdatingEventHandler(adapRowUpdatingEventHandler); //debug
         }
 
+        /// <summary>
+        /// appeler si suppression d'elements
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void dataAdap_RowUpdated(object sender, MySqlRowUpdatedEventArgs e)
         {
-            if (e.RecordsAffected == 1 && e.Command == dataAdap.InsertCommand)
+            if (e.RecordsAffected == 1 && e.Command == dataAdap.InsertCommand)//si une ligne a été modifié et que la commande sql est un insert
             {
                 //SET LAST_INSERT_ID();
                 MySqlCommand idQuery = connex.CreateCommand();
-                idQuery.CommandText = "SELECT LAST_INSERT_ID();";
+                idQuery.CommandText = "SELECT LAST_INSERT_ID();"; //renvoie l'id du dernier enregistement
                 int id = (int)(ulong)idQuery.ExecuteScalar();
                 dataAdap.InsertCommand.Parameters["@id"].Value = id;
-                e.Row["id"] = id;
+                e.Row["id"] = id; // affiche l'id dans la collone id
             }
         }
 
+        /// <summary>
+        /// appeler si un changement à lieu sur une ligne
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void table_RowChanged(object sender, DataRowChangeEventArgs e)
         {
             //connex.CancelQuery(0);
             dataAdap.Update((DataTable)((BindingSource)dataGridView1.DataSource).DataSource);
         }
 
+        //debug
         /*void adapRowUpdatingEventHandler(
 	        Object sender,
 	        MySqlRowUpdatingEventArgs e
